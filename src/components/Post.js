2820a4aa51a49dd009c7c16cb3372be01d1c3767
Mikael <miketekle14@gmail.com>
2017-01-19 01:21:31 -0800

    refactored to be functional component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,28 +1,13 @@
-import React, { Component } from 'react';
-import { TouchableWithoutFeedback, TouchableOpacity } from 'react-native';
-import { Actions } from 'react-native-router-flux';
+import React from 'react';
 import ReusablePostView from './ReusablePostView';
 
-class Post extends Component {
-
-  toDetail() {
-    const { post } = this.props;
-    Actions.detail({ post });
-  }
-
-  render() {
-    return (
-      <TouchableOpacity
-        onPress={this.toDetail.bind(this)}
-        activeOpacity={1}
-      >
-        <ReusablePostView
-          post={this.props.post}
-          addLikeButton={true}
-        />
-      </TouchableOpacity>
-    );
-  }
-}
+const Post = ({ post }) => {
+  return (
+    <ReusablePostView
+      post={post}
+      timelineView
+    />
+  );
+};
 
 export default Post;
